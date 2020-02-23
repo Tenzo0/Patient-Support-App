6009ee8310507ad4d7341ebb83344a3272717e97
Alexey <aid.dev@ya.ru>
2020-02-23 19:16:38 +0400

    added first version of recommendation layout

--- app/src/main/java/ru/poas/patientassistant/client/ui/main/RecommendationsFragment.kt
+++ app/src/main/java/ru/poas/patientassistant/client/ui/main/RecommendationsFragment.kt
@@ -1,33 +1,38 @@
 package ru.poas.patientassistant.client.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
-
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
 import ru.poas.patientassistant.client.R
+import ru.poas.patientassistant.client.databinding.RecommendationsFragmentBinding
 import ru.poas.patientassistant.client.viewmodel.main.RecommendationsViewModel
 
 class RecommendationsFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = RecommendationsFragment()
-    }
-
     private lateinit var viewModel: RecommendationsViewModel
+    private lateinit var binding: RecommendationsFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.recommendations_fragment, container, false)
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.recommendations_fragment, container, false)
+
+        activity!!.toolbar.apply {
+            title = "Рекомендации"
+        }
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(RecommendationsViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(RecommendationsViewModel::class.java)
         // TODO: Use a ViewModel
     }
 
