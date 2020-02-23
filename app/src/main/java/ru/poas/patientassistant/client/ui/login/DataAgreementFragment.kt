63fe4e2dadfb43343282893502b3fa9755c3f591
Alexey <aid.dev@ya.ru>
2020-02-23 17:55:44 +0400

    added ugly decline data dialog

--- app/src/main/java/ru/poas/patientassistant/client/ui/login/DataAgreementFragment.kt
+++ app/src/main/java/ru/poas/patientassistant/client/ui/login/DataAgreementFragment.kt
@@ -1,15 +1,17 @@
 package ru.poas.patientassistant.client.ui.login
 
+import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import ru.poas.patientassistant.client.R
 import ru.poas.patientassistant.client.databinding.DataAgreementFragmentBinding
-
 
 class DataAgreementFragment : Fragment() {
 
@@ -23,14 +25,49 @@ class DataAgreementFragment : Fragment() {
         //If user accept law about personal data
         binding.acceptButton.setOnClickListener {
             //Navigate to MainActivity
-            findNavController().navigate(
-                DataAgreementFragmentDirections.actionDataAgreementFragmentToMainActivity()
-            )
-            //Finish LoginActivity
-            activity!!.finish()
+            startMainActivity()
+        }
+
+        //if decline
+        val declineDialog = createDeclineDialog()
+        binding.declineButton.setOnClickListener {
+            declineDialog.show()
         }
 
         return binding.root
     }
 
+    private fun createDeclineDialog(): Dialog = AlertDialog.Builder(requireContext())
+        .setTitle("Выход из приложения")
+        .setMessage("При несогласии на обработку персональных данных, " +
+            "вы не сможете использовать приложение.")
+        .setPositiveButton("Согласиться", myClickListener)
+        .setNegativeButton("выйти", myClickListener)
+        .setNeutralButton("Отмена", myClickListener)
+        .create()
+
+    private var myClickListener: DialogInterface.OnClickListener =
+        DialogInterface.OnClickListener { dialog, which ->
+            when (which) {
+                // accept button
+                Dialog.BUTTON_POSITIVE -> {
+                    startMainActivity()
+                }
+                // decline button
+                Dialog.BUTTON_NEGATIVE -> activity!!.finish()
+            }
+        }
+
+    private fun startMainActivity() {
+        //Navigate to MainActivity
+        findNavController().navigate(
+            DataAgreementFragmentDirections.actionDataAgreementFragmentToMainActivity()
+        )
+        //Finish LoginActivity
+        activity!!.finish()
+    }
+
+    companion object {
+        //private const val DIALOG_EXIT = 1
+    }
 }
