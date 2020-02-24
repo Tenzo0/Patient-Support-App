51bd1944a44385a3a06c65e9a0ff1079e6e507bf
Alexey <aid.dev@ya.ru>
2020-02-24 13:10:02 +0400

    added calendar in toolbar in recommendationsFragment

--- app/src/main/java/ru/poas/patientassistant/client/ui/main/RecommendationsFragment.kt
+++ app/src/main/java/ru/poas/patientassistant/client/ui/main/RecommendationsFragment.kt
@@ -1,16 +1,21 @@
 package ru.poas.patientassistant.client.ui.main
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.calendar_button
+import kotlinx.android.synthetic.main.activity_main.view.*
 import ru.poas.patientassistant.client.R
 import ru.poas.patientassistant.client.databinding.RecommendationsFragmentBinding
 import ru.poas.patientassistant.client.viewmodel.main.RecommendationsViewModel
+import java.util.*
 
 class RecommendationsFragment : Fragment() {
 
@@ -23,8 +28,21 @@ class RecommendationsFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(layoutInflater, R.layout.recommendations_fragment, container, false)
 
+        val cldr = Calendar.getInstance()
+        val day = cldr.get(Calendar.DAY_OF_MONTH)
+        val month = cldr.get(Calendar.MONTH)
+        val year = cldr.get(Calendar.YEAR)
+
+        val picker = DatePickerDialog(activity!!,
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            }, year, month, day)
+
         activity!!.toolbar.apply {
             title = "Рекомендации"
+        }
+        activity!!.calendar_button.visibility = View.VISIBLE
+        activity!!.calendar_button.setOnClickListener {
+            picker.show()
         }
 
         return binding.root
