7914a8261918fc66a105dfde01f68f19ffe915b7
vitek999 <vitek2012rus@gmail.com>
2020-02-27 15:42:34 +0400

    added getting recommendation id for getting glossary

--- app/src/main/java/ru/poas/patientassistant/client/repository/GlossaryRepository.kt
+++ app/src/main/java/ru/poas/patientassistant/client/repository/GlossaryRepository.kt
@@ -6,10 +6,12 @@ import androidx.lifecycle.Transformations
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import ru.poas.patientassistant.client.api.GlossaryNetwork
+import ru.poas.patientassistant.client.api.RecommendationNetwork
 import ru.poas.patientassistant.client.db.glossary.GlossaryDatabase
 import ru.poas.patientassistant.client.db.glossary.asDomainModel
 import ru.poas.patientassistant.client.preferences.UserPreferences
 import ru.poas.patientassistant.client.vo.GlossaryItem
+import ru.poas.patientassistant.client.vo.User
 import ru.poas.patientassistant.client.vo.asDatabaseModel
 
 class GlossaryRepository(private val database: GlossaryDatabase) {
@@ -22,13 +24,19 @@ class GlossaryRepository(private val database: GlossaryDatabase) {
      */
     suspend fun refreshGlossary(credentials: String) {
         withContext(Dispatchers.IO) {
-            val glossary = GlossaryNetwork.glossaryService
-                .getGlossary(credentials, UserPreferences.getRecommendationId())
-                .body()
-            Log.i("tag4", glossary.toString())
-            database.glossaryDao.clear()
-            glossary?.glossaryItems?.asDatabaseModel()?.let {
-                database.glossaryDao.insert(it)
+            val recommendation = RecommendationNetwork.recommendationService.getUserRecommendationsByPatientId(
+                credentials, UserPreferences.getId()
+            ).body()
+
+            recommendation?.let {
+                val glossary = GlossaryNetwork.glossaryService
+                    .getGlossary(credentials, it.recommendationId)
+                    .body()
+                Log.i("tag4", glossary.toString())
+                database.glossaryDao.clear()
+                glossary?.glossaryItems?.asDatabaseModel()?.let {
+                    database.glossaryDao.insert(it)
+                }
             }
         }
     }
