6bad4f02671dcfe5a2a7882785be49c62acaa68d
Alexey <alexey5500@ya.ru>
2020-03-02 16:27:16 +0000

    Revert "added empty di classes"
    This reverts commit accd709694df990cc6eea6957c34334bcb86d51b

--- app/src/main/java/ru/poas/patientassistant/client/B2DocApplication.kt
+++ app/src/main/java/ru/poas/patientassistant/client/B2DocApplication.kt
@@ -1,25 +1,10 @@
 package ru.poas.patientassistant.client
 
 import android.app.Application
-import ru.poas.patientassistant.client.di.AppComponent
-import ru.poas.patientassistant.client.di.DaggerAppComponent
 import timber.log.Timber
 import timber.log.Timber.DebugTree
 
-
-open class B2DocApplication: Application() {
-
-    // Instance of the AppComponent that will be used by all the Activities in the project
-    val appComponent: AppComponent by lazy {
-        initializeComponent()
-    }
-
-    open fun initializeComponent(): AppComponent {
-        // Creates an instance of AppComponent using its Factory constructor
-        // We pass the applicationContext that will be used as Context in the graph
-        return DaggerAppComponent.factory().create(applicationContext)
-    }
-
+class B2DocApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
--- app/src/main/java/ru/poas/patientassistant/client/ui/login/di/LoginComponent.kt
+++
@@ -1,4 +0,0 @@
-package ru.poas.patientassistant.client.ui.login.di
-
-interface LoginComponent {
-}
