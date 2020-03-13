517dbd208049f4c68555cc55f17439aac8cdc9fa
Alexey <alexey5500@ya.ru>
2020-03-13 04:31:16 +0000

    Update .ci-notify.sh

--- .ci-notify.sh
+++ .ci-notify.sh
@@ -5,5 +5,5 @@ URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
 TEXT="Deploy status: $1%0A%0A📕Project:+$CI_PROJECT_NAME%0A🌏URL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0A📌Branch:+$CI_COMMIT_REF_SLUG%0A📋Commit:+$CI_COMMIT_TITLE%0A➡️Commit URL:+$CI_PROJECT_URL/commit/$CI_COMMIT_SHA/%0A%0A🙎‍♂️Job has been started by $GITLAB_USER_LOGIN"
 
 curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
-curl -s —max-time $TIME -d "chat_id=$TELEGRAM_USER_ID_2&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null 
-curl -s —max-time $TIME -d "chat_id=$TELEGRAM_USER_ID_3&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
+curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID_2&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null 
+curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID_3&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
