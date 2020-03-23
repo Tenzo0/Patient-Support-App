f790cbe87aa604adbd7e47fdd47c02e7ad9f2204
Alexey <aid.dev@ya.ru>
2020-03-24 01:32:25 +0400

    fix incorrect drug date parsing

--- app/src/main/java/ru/poas/patientassistant/client/utils/DateConstants.kt
+++ app/src/main/java/ru/poas/patientassistant/client/utils/DateConstants.kt
@@ -4,9 +4,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 object DateConstants{
-    val DATABASE_DATE_FORMAT = SimpleDateFormat("yyyy-MM-dd", Locale("ru", "RU")).apply {
-        timeZone = TimeZone.getTimeZone("GMT")
-    }
+    val DATABASE_DATE_FORMAT = SimpleDateFormat("yyyy-MM-dd", Locale("ru", "RU"))
     val DATABASE_TIME_FORMAT = SimpleDateFormat("HH:mm:ss", Locale("ru", "RU")).apply {
         timeZone = TimeZone.getTimeZone("GMT")
     }
