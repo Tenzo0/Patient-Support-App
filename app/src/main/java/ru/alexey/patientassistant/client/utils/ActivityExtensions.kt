7941a2a14941f91b7b84c35b46327bffaa1b5aa3
Alexey Barykin <alexey5500@ya.ru>
2020-06-18 07:13:14 +0400

    revert keyboard hiding changes

--- app/src/main/java/ru/alexey/patientassistant/client/utils/ActivityExtensions.kt
+++ app/src/main/java/ru/alexey/patientassistant/client/utils/ActivityExtensions.kt
@@ -16,7 +16,6 @@ fun Activity.hideKeyboard() {
             this.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
         val view = (if (this.currentFocus != null) this.currentFocus else this) as View
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
-        view.clearFocus()
     } catch (e: Exception) {
         Timber.e(e)
     }
